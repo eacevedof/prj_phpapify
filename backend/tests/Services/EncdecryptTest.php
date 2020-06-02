14a5398f4714f5cde04f1d3b975db4ee5940d00e
ioedumac <eacevedof@yahoo.es>
2020-06-02 21:29:14 +0200

    no pasa el test is valid excepcion localhost:200

--- backend/tests/Services/EncdecryptTest.php
+++ backend/tests/Services/EncdecryptTest.php
@@ -9,9 +9,11 @@ use App\Services\Apify\SignService;
 $pathappboot = realpath(__DIR__."/../../boot/appbootstrap.php");
 include_once($pathappboot);
 
-
 class EncdecryptTest extends TestCase
-{    
+{
+
+    private $token;
+
     private function log($mxVar,$sTitle=NULL)
     {
         $oLog = new ComponentLog("logs",__DIR__);
@@ -23,11 +25,27 @@ class EncdecryptTest extends TestCase
         $post=["user"=>"fulanito","password"=>"menganito"];
         $oServ = new SignService("localhost:200",$post);
         $token = $oServ->get_token();
-        print_r($token);
+        $this->token = $token;
         $this->assertIsString($token);
     }
 
-    
+    /**
+     * @depends test_get_token
+     */
+    public function test_is_valid()
+    {
+        $post=["user"=>"fulanito","password"=>"menganito"];
+        $oServ = new SignService("localhost:200",$post);
+        $r = $oServ->is_valid($this->token);
+        $this->assertEquals(true,$r);
+    }
 
+    /**
+     * @depends test_get_token
+     */
+    public function tes_is_invalid()
+    {
+
+    }
     
 }//EncdecryptTest
