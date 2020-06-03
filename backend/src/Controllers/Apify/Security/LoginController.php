7571776660960a39cfa67ad01cf754f4bc5916e8
ioedumac <eacevedof@yahoo.es>
2020-06-03 19:54:14 +0200

    configurando login

---
+++ backend/src/Controllers/Apify/Security/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link www.eduardoaf.com
+ * @name App\Controllers\Apify\LoginController
+ * @file LoginController.php 1.0.0
+ * @date 03-06-2020 18:17 SPAIN
+ * @observations
+ */
+namespace App\Controllers\Apify\Security;
+
+use TheFramework\Helpers\HelperJson;
+use App\Controllers\AppController;
+use App\Services\Apify\Security\SignatureService;
+
+class LoginController extends AppController
+{
+
+    /**
+     * ruta:
+     *  <dominio>/apifiy/security/login
+     */
+    public function index()
+    {
+
+
+    }//index
+
+}//LoginController
---
+++ backend/src/Services/Apify/Security/LoginService.php
@@ -0,0 +1,112 @@
+<?php
+namespace App\Services\Apify\Security;
+use Matrix\Exception;
+use TheFramework\Components\Session\ComponentEncdecrypt;
+
+class LoginService
+{
+    private $domain = null;
+    private $data = null;
+    /**
+     * @var ComponentEncdecrypt
+     */
+    private $encdec = null;
+
+    public function __construct($domain,$data)
+    {
+        $this->domain = $domain;
+        $this->data = $data;
+        $this->_load_encdec();
+    }
+
+    private function _load_encdec()
+    {
+        $config = $this->_get_config($this->domain);
+        if(!$config)
+            throw new \Exception("Domain {$this->domain} is not authorized");
+
+        $this->encdec = new ComponentEncdecrypt(1);
+        $this->encdec->set_sslmethod($config["sslenc_method"]??"");
+        $this->encdec->set_sslkey($config["sslenc_key"]??"");
+        $this->encdec->set_sslsalt($config["sslsalt"]??"");
+    }
+
+    private function _get_config()
+    {
+        $sPathfile = $_ENV["APP_ENCDECRYPT"] ?? __DIR__.DIRECTORY_SEPARATOR."encdecrypt.json";
+        if(!is_file($sPathfile)) {
+            throw new \Exception("No encdecrypt file found: $sPathfile");
+        }
+        $sJson = file_get_contents($sPathfile);
+        $arconfig = json_decode($sJson,1);
+        foreach ($arconfig as $arconf)
+        {
+            if($arconf["domain"]===$this->domain)
+                return $arconf;
+        }
+        return [];
+    }
+
+    private function to_string($data)
+    {
+        if(is_string(data))
+            return data;
+        return var_export(data,1);
+    }
+
+    public function get_token()
+    {
+        $data = var_export($this->data,1);
+        $package = [
+            "domain"   => $this->domain,
+            "remoteip" => $this->_get_remote_ip(),
+            "hash"     => md5($data),
+            "today"    => date("Ymd"),
+        ];
+
+        $instring = implode("-",$package);
+        //print("package to encrypt:\n");
+        //print_r($package);
+        //print_r("to encrypt:  {$instring}");
+        $token = $this->encdec->get_sslencrypted($instring);
+        return $token;
+    }
+
+    private function _get_remote_ip()
+    {
+        return $_SERVER["REMOTE_ADDR"]  ?? "127.0.0.1";
+    }
+
+    private function validate_package($arpackage)
+    {
+        $data = var_export($this->data,1);
+        $md5 = md5($data);
+
+        if(!$arpackage)
+            throw new Exception("Wrong token submitted");
+
+        if($arpackage[0]!==$this->domain)
+            throw new Exception("Domain {$this->domain} not Authorized");
+
+        if($arpackage[1]!==$this->_get_remote_ip())
+            throw new Exception("Wrong source {$arpackage[0]} in token");
+
+        if($arpackage[2]!==$md5)
+            throw new Exception("Wrong hash submitted");
+
+        if($arpackage[3]!==date("Ymd"))
+            throw new Exception("token has expired");
+    }
+
+    public function is_valid($token)
+    {
+        //print_r("\ntoken:{$token}");
+        $instring = $this->encdec->get_ssldecrypted($token);
+        $package = explode("-",$instring);
+        //print_r("exploded:");
+        //print_r($package);
+        //esto lanza expecipones
+        $this->validate_package($package);
+        return true;
+    }
+}
