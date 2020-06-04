ed994479b49a3200683f789195b7c8b77ed5922e
ioedumac <eacevedof@yahoo.es>
2020-06-04 19:42:29 +0200

    loginservicetest ok

--- backend/tests/Services/Apify/Security/LoginServiceTest.php
+++ backend/tests/Services/Apify/Security/LoginServiceTest.php
@@ -22,11 +22,44 @@ class LoginServiceTest extends TestCase
      * @expectedException \Exception
      * @expectedExceptionMessage Source domain not authorized
      */
-    public function test_get_token()
+    public function test_get_token_nok()
     {
         $post=["user"=>"fulanito","password"=>"menganito"];
         $oServ = new LoginService("localhost:200",$post);
         $oServ->get_token();
+    }
+
+    public function test_get_token_ok()
+    {
+        $post=["user"=>"fulanito","password"=>"menganito"];
+        $oServ = new LoginService("*",$post);
+        $token = $oServ->get_token();
+        $this->assertNotEmpty($token);
+    }
+
+    public function test_is_valid_token_ok()
+    {
+        $post=["user"=>"fulanito","password"=>"menganito"];
+        $oServ = new LoginService("*",$post);
+        $token = $oServ->get_token();
+        $oServ = new LoginService("*");
+        $isvalid = $oServ->is_valid($token);
+        $this->assertTrue($isvalid);
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage Wrong token submitted
+     */
+    public function test_valid_token_nok()
+    {
+        $post=["user"=>"fulanito","password"=>"menganito"];
+        $oServ = new LoginService("*",$post);
+        $token = $oServ->get_token();
+        $token .= "xxxx";
+
+        $oServ = new LoginService("*");
+        $oServ->is_valid($token);
     }
 
 
